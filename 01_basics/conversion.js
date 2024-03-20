3b9157a58d722d5c2c9214a8933d039f0f3f8700
Daniel Scott <mr.danielscottofficial@gmail.com>
2024-03-20 16:02:36 +0500

    operators

---
+++ 01_basics/conversion.js
@@ -0,0 +1,67 @@
+// let age = "dani";
+
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "45" => 45
+// "33avbf" => NaN
+// true => 1; 
+// false => 0
+
+
+let isLoggedIn = "fg"
+
+// let booleanisloggedIN = Boolean(isLoggedIn)
+// console.log(booleanisloggedIN);
+
+// 1 => true; 0 => false
+// "" => false
+// "kuch lik deya" => true
+
+let someNumber = 33
+
+let strnum = String(someNumber)
+// console.log(typeof strnum);
+// console.log(strnum);
+
+// ************************************************* operators ****************************///
+
+let firnum = 32
+let negvalue = -firnum
+// console.log(negvalue);
+
+
+// *********** Basic operation 
+// console.log(1+2);
+// console.log(1-2);
+// console.log(1*2);
+// console.log(1**2);
+// console.log(1/2);
+// console.log(1%2);
+
+let str1 = "Hello"
+let str2 = " World"
+let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 5) * (2) % (23));
+
+
+// console.log(+true);
+// console.log(-true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameConuter = 100
+++gameConuter;
+ 
+// console.log(gameConuter);
