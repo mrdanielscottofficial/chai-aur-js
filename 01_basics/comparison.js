6dac598649f0ae56187f986733f7ca317552597f
Daniel Scott <mr.danielscottofficial@gmail.com>
2024-03-21 14:49:34 +0500

    Comparison in js

---
+++ 01_basics/comparison.js
@@ -0,0 +1,26 @@
+// comparison in js
+
+// console.log(3 > 24);
+// console.log(53 < 64);
+// console.log(24 == 43);
+// console.log(22 === 67);
+// console.log(57 != 35) ;
+
+
+// console.log("35" > 5);
+// console.log("35" < 5);
+// console.log("35" < true);
+// console.log("35" != false);
+
+// console.log(null > 8);
+// console.log(null < 8);
+// console.log(null == 8);
+
+
+console.log(undefined = null);
+console.log(undefined == null);
+console.log(undefined > null);
+console.log(undefined < null);
+
+// ===
+console.log("hello " === "rfs");
